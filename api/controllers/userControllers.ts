2d61b2fabbc65f5622e4ae7abd396027919e955e
mendingbook <jesse.robinson004@gmail.com>
2022-08-08 13:47:40 -0500

    Created user login route

--- api/controllers/userControllers.ts
+++ api/controllers/userControllers.ts
@@ -20,3 +20,19 @@ export const registerUser = async (req: Request, res: Response) => {
     res.status(409).json({ message: error });
   }
 };
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await userModel.findOne({ username: username });
+  const encryptedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("base64");
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  if (user.password != encryptedPassword) {
+    return res.status(401).json({ message: "Incorrect password" });
+  }
+  res.status(200).json(user);
+};
--- api/models/userModels.ts
+++ api/models/userModels.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 import { userDB } from "../../config/connections";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   articles: [String],
-  joined: { type: Date, defualt: new Date() },
+  joined: { type: Date, defualt: Date.now },
 });
 
 export var userModel = userDB.model("user", userSchema);
