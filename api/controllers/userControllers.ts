ecaaba3212f027bb3e519f4c8186b43a1fd8c806
mendingbook <jesse.robinson004@gmail.com>
2022-08-08 13:46:32 -0500

    Created register user route

--- api/controllers/userControllers.ts
+++ api/controllers/userControllers.ts
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
 import { Request, Response } from "express";
 import { userModel } from "../models/userModels";
+import crypto from "crypto";
 
-export const registerUser = async () => {};
-export const loginUser = async () => {};
-export const getUser = async () => {};
-export const deleteUser = async () => {};
+export const registerUser = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const encryptedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("base64");
+  const user = new userModel({
+    username,
+    password: encryptedPassword,
+  });
+  try {
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(409).json({ message: error });
+  }
+};
