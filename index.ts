507a031d33b1f72aa77b021a334de74325907875
mendingbook <jesse.robinson004@gmail.com>
2022-08-10 21:48:52 -0500

    Added cors middleware

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./api/routes/userRoutes";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,6 +9,13 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json({ limit: "30mb" }));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
+  })
+);
 
 app.use("/user", userRoutes);
 
