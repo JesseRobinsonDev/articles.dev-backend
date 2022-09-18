d1abb31d5867c20c85360172fe4e62120fd68c79
mendingbook <jesse.robinson004@gmail.com>
2022-09-17 23:14:02 -0500

    Added comments to explain routes

--- api/routes/userRoutes.ts
+++ api/routes/userRoutes.ts
@@ -10,11 +10,22 @@ import {
 
 const router = express.Router();
 
+// Register user route, query parameters: Username, Password
 router.post("/register", registerUser);
+
+// Login user route, query parameters: Username, Password
 router.post("/login", loginUser);
+
+// Get user route, url parameters: UserID
 router.get("/:id/get", getUser);
+
+// Searches for articles made by the user, url parameters: UserID, query parameters: Limit, Offset
 router.get("/:id/articles/search", searchUserArticles);
+
+// Searches for comments made by the user, url parameters: UserID, query parameters: Limit, Offset
 router.get("/:id/comments/search", searchUserComments);
+
+// Deletes the user, url parameters: UserID
 router.delete("/:id/delete", deleteUser);
 
 export default router;
